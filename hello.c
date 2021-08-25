b31444ba2fc7e1a588a4521e5c3845c585050ea4
Hanna Tran <Htran38@huskers.unl.edu>
2021-08-25 23:52:47 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Authour: Hanna Tran, Lindsey Wiegert 
+ * date: 2021/08/24
+ * 
+ * a simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv){
+     
+    printf("Hello world!\n");
+    
+    return 0;
+ }
